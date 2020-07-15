0534f27f589499080134252a37eae550ba90e026
yasmin_araujo <yasminoa@hotmail.com>
2020-07-15 10:46:55 -0300

    Upload Project

---
+++ ProjetoFinal/src/TelaInicialGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class TelaInicialGUI
+{
+    private JButton btnGuiaFinanceiro;
+    private JButton btnDicionarioFinanceiro;
+    private JButton btnIniciarCalculo;
+    private JButton btnInstrucoes;
+    private JLabel lblTitulo;
+}
+
+// tão me vendo? respondam aqui pls intellij agora vao ter q me esparar vcs que lutem kk
+//
